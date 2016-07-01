400daaf604cc2c57716985b88000b722eb32420c
pajamaw <pjpeterww@gmail.com>
2016-07-01 14:59:02 -0400

    Done.

--- test.js
+++ test.js
@@ -1,14 +1,15 @@
-
 var expect = require('chai').expect
+
 var name = 'React Quickly'
 var url = ['http://reactquickly.co', 'https://www.manning.com/books/react-quickly']
+
 describe('name and url', function() {
-  it('must match the values', function(done){
+  it('must be a string', function(done){
     expect(name).to.be.a('string')
     expect(name).to.equal('React Quickly')
+
     expect(url).to.have.length(2)
-    expect(url).to.have.deep.property('[1]', 'https://www.manning.com/books/react-quickly')
-      .with.length(43)
+    expect(url).to.have.deep.property('[1]', 'https://www.manning.com/books/react-quickly').with.length(43)
     done()
   })
-})
+});
